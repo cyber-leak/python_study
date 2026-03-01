b3f0ee4035402f6d8e6cc126a26e63cfa29e5b9f
Daysi-ibs <ilya.bazarov72@gmail.com>
2026-03-01 19:26:43 +0200

    Adds inline menu with callback handling
    Introduces an interactive inline keyboard menu allowing users to respond to a programming interest prompt, with tailored feedback based on their selection.
    Fixes handler function typos and improves help and goodbye responses for better user experience.

--- Telegramm_bot/bot.py
+++ Telegramm_bot/bot.py
@@ -51,16 +51,46 @@ def answer_hello(message):
     bot.send_message(message.chat.id, "и тебе привет, дорогой пользоватеь!")
 
 
-@bot.message_handler(fucn=lambda message: message.text == "Помощь")
+@bot.message_handler(func=lambda message: message.text == "Помощь")
 def help_command(message):
     bot.send_message(
-        message.chat.id, "Я - учебный Бот. Пока я не умею только здороваться!"
+        message.chat.id, "Я - учебный Бот. Пока я умею только здороваться!"
     )
 
 
-@bot.message_handler(func=lambda message: message == "Пока")
+@bot.message_handler(func=lambda message: message.text == "Пока")
 def goodbuy_message(message):
     bot.send_message(message.chat.id, "и тебе досвидания мой дорогой друг!")
+
+
+# add commands menu
+@bot.message_handler(commands=["menu"])
+def show_inline_menu(message):
+    # Создаем объект клавиатуры
+    markup = types.InlineKeyboardMarkup()
+
+    # Создаем кнопки.
+    # text — что видит юзер, callback_data — что "слышит" бот
+    btn_yes = types.InlineKeyboardButton(text="Да", callback_data="yes")
+    btn_no = types.InlineKeyboardButton(text="Нет", callback_data="no")
+
+    # Добавляем кнопки в ряд
+    markup.add(btn_yes, btn_no)
+
+    bot.send_message(
+        message.chat.id, "Тебе нравится изучать программирование", reply_markup=markup
+    )
+
+
+# 2. Создаем обработчик нажатий (callback)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    # call.data — это то самое значение "yes" или "no"
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "Круто! Продолжай в том же духе!")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Ничего, скоро втянешься!")
+    bot.answer_callback_query(call.id)
 
 
 # Запуск бота
